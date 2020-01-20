3d4ee601a3c4b50839e1bc124289ab44c7f749a8
sousajo <josesousa@rentalcars.com>
2020-01-20 12:21:52 +0000

    v1.1 tax calculation

--- exercises/java/src/main/java/tax/DefaultTaxCalculator.java
+++ exercises/java/src/main/java/tax/DefaultTaxCalculator.java
@@ -21,25 +21,25 @@ public class DefaultTaxCalculator extends TaxCalculator{
 
        if(vehicle.getCo2Emissions() > 1 || vehicle.getCo2Emissions() < 50){
            return 10;
-       }else if (vehicle.getCo2Emissions() > 51 || vehicle.getCo2Emissions() < 75){
+       }else if (vehicle.getCo2Emissions() > 51 && vehicle.getCo2Emissions() < 75){
            return 25;
-       }else if (vehicle.getCo2Emissions() > 76 || vehicle.getCo2Emissions() < 90){
+       }else if (vehicle.getCo2Emissions() > 76 && vehicle.getCo2Emissions() < 90){
            return 105;
-       }else if (vehicle.getCo2Emissions() > 91 || vehicle.getCo2Emissions() < 100){
+       }else if (vehicle.getCo2Emissions() > 91 && vehicle.getCo2Emissions() < 100){
            return 125;
-       }else if (vehicle.getCo2Emissions() > 101 || vehicle.getCo2Emissions() < 110){
+       }else if (vehicle.getCo2Emissions() > 101 && vehicle.getCo2Emissions() < 110){
            return 145;
-       }else if (vehicle.getCo2Emissions() > 111 || vehicle.getCo2Emissions() < 130){
+       }else if (vehicle.getCo2Emissions() > 111 && vehicle.getCo2Emissions() < 130){
            return 165;
-       }else if (vehicle.getCo2Emissions() > 131 || vehicle.getCo2Emissions() < 150){
+       }else if (vehicle.getCo2Emissions() > 131 && vehicle.getCo2Emissions() < 150){
            return 205;
-       }else if (vehicle.getCo2Emissions() > 151 || vehicle.getCo2Emissions() < 170){
+       }else if (vehicle.getCo2Emissions() > 151 && vehicle.getCo2Emissions() < 170){
            return 515;
-       }else if (vehicle.getCo2Emissions() > 171 || vehicle.getCo2Emissions() < 190){
+       }else if (vehicle.getCo2Emissions() > 171 && vehicle.getCo2Emissions() < 190){
            return 830;
-       }else if (vehicle.getCo2Emissions() > 191 || vehicle.getCo2Emissions() < 225){
+       }else if (vehicle.getCo2Emissions() > 191 && vehicle.getCo2Emissions() < 225){
            return 1240;
-       }else if (vehicle.getCo2Emissions() > 226 || vehicle.getCo2Emissions() < 255){
+       }else if (vehicle.getCo2Emissions() > 226 && vehicle.getCo2Emissions() < 255){
            return 1760;
        }else if (vehicle.getCo2Emissions() > 255){
            return 2070;
